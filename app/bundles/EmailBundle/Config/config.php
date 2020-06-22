8354dc4f01718756afba24a9688c1d23b369ebd1
Islam Mohamed <65309837+Wise-Gorilla@users.noreply.github.com>
2020-06-22 14:58:49 +0200

    Update config.php

--- app/bundles/EmailBundle/Config/config.php
+++ app/bundles/EmailBundle/Config/config.php
@@ -813,6 +813,7 @@ return [
         'mailer_encryption'            => null, //tls or ssl,
         'mailer_auth_mode'             => null, //plain, login or cram-md5
         'mailer_amazon_region'         => 'email-smtp.us-east-1.amazonaws.com',
+        'mailer_amazon_api_region'     => 'us-east-1',
         'mailer_custom_headers'        => [],
         'mailer_spool_type'            => 'memory', //memory = immediate; file = queue
         'mailer_spool_path'            => '%kernel.root_dir%/../var/spool',
